cad9fc5c2ce9ca6724d15aca9ab7dcbe7f156d8b
Alexander <belyanskii@ya.ru>
2016-07-03 15:33:33 +0300

    vcard updates

--- Vcard/Bem/desktop.blocks/p-vcard/p-vcard.bemhtml.js
+++ Vcard/Bem/desktop.blocks/p-vcard/p-vcard.bemhtml.js
@@ -1,4 +1,63 @@
 ﻿block('p-vcard').replace()(function () {
+    var data = {};
+
+    data.order = [];
+    data.favicond = {
+        ru: '//yastatic.net/morda-logo/i/favicon_islands.ico',
+        en: '//yastatic.net/morda-logo/i/favicon_comtr.ico'
+    };
+    data.cards = {
+        ru: {
+            lang: 'ru',
+            name: 'Иван Иванов',
+            position: 'Разработчик интерфейсов',
+            address: {
+                country: 'Россия',
+                city: 'Москва',
+                zip: '119021',
+                'street-address': 'ул. Льва Толстого, д. 16'
+            },
+            company: {
+                name: 'Яндекс',
+                site: 'https://yandex.ru'
+            },
+            contact: {
+                work: '+7 (495) 739-70-00',
+                workExt: '0000',
+                cell: '+7 (555) 123-45-66',
+                email: 'ivanivanovich@yandex-team.ru',
+                site: 'https://ivanivanovich.ru',
+                github: 'ivanivanovich',
+                twitter: 'ivanivanovich',
+                skype: 'ivanivanovich'
+            }
+        },
+        en: {
+            lang: 'en',
+            name: 'Ivan Ivanov',
+            position: 'UI Developer',
+            address: {
+                country: 'Russia',
+                city: 'Moscow',
+                zip: '119021',
+                'street-address': '16, Leo Tolstoy St.'
+            },
+            company: {
+                name: 'Yandex',
+                site: 'https://yandex.com'
+            },
+            contact: {
+                work: '+7 (495) 739-70-00',
+                workExt: '000',
+                cell: '+7 (555) 123-45-67',
+                email: 'ivanivanovich@yandex-team.ru',
+                site: 'https://ivanivanovich.ru/#en',
+                skype: 'ivanivanovich',
+                github: 'ivanivanovich',
+                twitter: 'ivanivanovich'
+            }
+        }
+    };
 
     return {
         block: 'page',
@@ -6,10 +65,15 @@
         head: [
             { elem: 'meta', attrs: { name: 'description', content: '' } },
             { elem: 'meta', attrs: { name: 'viewport', content: 'width=device-width, initial-scale=1' } },
-            { elem: 'css', url: 'default.min.css' }
+            { elem: 'css', url: '/Bem/desktop.bundles/default/default.css' }
         ],
-        scripts: [{ elem: 'js', url: 'default.min.js' }],
+        scripts: [{ elem: 'js', url: '/Bem/desktop.bundles/default/default.js' }],
         mix: { block: 'p-vcard' },
-        content: 'Hello world!'
+        content: {
+            block: 'card',
+            order: data.order,
+            cards: data.cards,
+            favicons: data.favicons
+        }
     };
 });
--- Vcard/Bem/desktop.bundles/default/default.browser.js
+++ Vcard/Bem/desktop.bundles/default/default.browser.js
@@ -417,3 +417,245 @@ else {
 
 })(typeof window !== 'undefined' ? window : global);
 
+/* begin: ../../libs/bem-vcard-enb/blocks/page/page.js */
+/*global Card*/
+(function() {
+    var card, screen, isLandscape;
+
+    card = {
+        width: 600,
+        height: 360
+    };
+
+    screen = window.screen;
+
+    onOrientationChange();
+
+    window.addEventListener('orientationchange', onOrientationChange);
+
+    function getAvailWidth() {
+        return screen[ isLandscape ? 'availHeight' : 'availWidth' ];
+    }
+
+    function getAvailHeight() {
+        return screen[ isLandscape ? 'availWidth' : 'availHeight' ];
+    }
+
+    function getMetaViewport() {
+        return document.querySelector('meta[name=\"viewport\"]');
+    }
+
+    function setInitialScale(scale) {
+        return getMetaViewport().setAttribute('content', 'width=device-width, initial-scale=' + scale);
+    }
+
+    function getOrientation() {
+        return Math.abs(window.orientation) === 90;
+    }
+
+    function computeAndSetScale() {
+        var width = getAvailWidth();
+        var height = getAvailHeight();
+
+        if (width > card.width && card.height < height) {
+            return;
+        }
+
+        if (width < height) {
+            setInitialScale(width / card.width);
+        } else {
+            setInitialScale(height / card.height);
+        }
+    }
+
+    function onOrientationChange() {
+        isLandscape = getOrientation();
+        computeAndSetScale();
+    }
+}());
+
+document.addEventListener('DOMContentLoaded', function() {
+    Card.init();
+});
+
+/* end: ../../libs/bem-vcard-enb/blocks/page/page.js */
+/* begin: ../../libs/bem-vcard-enb/blocks/card/card.js */
+var Card = (function() {
+    var toArray = Array.prototype.slice;
+
+    var modSideOpened = 'card__side_state_opened';
+    var modSideClosed = 'card__side_state_closed';
+    var modAnimation = 'card_animation';
+    var modVisible = 'card_visible';
+    var modLinkDisabled = 'card__link_disabled_yes';
+
+    return {
+
+        init: function() {
+            this.card = document.querySelector('.card');
+            this.params = JSON.parse(this.card.dataset.bem).card;
+
+            this.sides = toArray.call(this.card.querySelectorAll('.card__side')).map(fillLang);
+            this.links = toArray.call(this.card.querySelectorAll('.card__switch .card__link')).map(fillLang);
+
+            // trying to fix google's markup tool
+            removeClass(this.sides[1].elem, modSideOpened);
+            addClass(this.sides[1].elem, modSideClosed);
+
+            window.addEventListener('hashchange', this._onHashChange.bind(this), false);
+
+            this._onHashChange();
+
+            setTimeout(function() {
+                addClass(Card.card, modAnimation);
+                addClass(Card.card, modVisible);
+            }, 0);
+
+            // drop phone links on desktop
+            if (!hasClass(document.documentElement, 'mobile')) {
+                setTimeout(function () {
+                    toArray.call(document.querySelectorAll('.card__phone-link')).forEach(function(elem) {
+                        elem.removeAttribute('href');
+                    });
+                }, 0);
+            }
+
+            function fillLang(elem) {
+                return {
+                    lang: elem.dataset.lang,
+                    elem: elem
+                };
+            }
+        },
+
+        _onHashChange: function() {
+            var lang = this._getLangFromHash();
+            if (lang) {
+                this.changeLang(lang);
+            }
+        },
+
+        changeLang: function(lang) {
+            this
+                ._changeTitle(lang)
+                ._changeFavicon(lang)
+                ._switchSide(lang)
+                ._changeUrl(lang);
+        },
+
+        _changeTitle: function(lang) {
+            document.title = this.params.titles[lang];
+            return this;
+        },
+
+        _changeFavicon: function(lang) {
+            document.querySelector('link[rel="shortcut icon"]').setAttribute('href', this.params.favicons[lang]);
+            return this;
+        },
+
+        _changeUrl: function(lang) {
+            this.links.forEach(function(link) {
+                if (link.lang === lang) {
+                    addClass(link.elem, modLinkDisabled);
+                } else {
+                    removeClass(link.elem, modLinkDisabled);
+                }
+            });
+
+            return this;
+        },
+
+        _switchSide: function(lang) {
+            var from;
+            var to;
+
+            if (this.sides[0].lang === lang) {
+                to = this.sides[0].elem;
+                from = this.sides[1].elem;
+            } else {
+                to = this.sides[1].elem;
+                from = this.sides[0].elem;
+            }
+
+            removeClass(to, modSideClosed);
+
+            if (hasClass(this.card, modAnimation)) {
+                setTimeout(doSwitch, 100);
+            } else {
+                // здесь нельзя просто setTimeout(cb, 100), т.к. в nextTick появится модификатор анимации
+                doSwitch();
+            }
+
+            function doSwitch() {
+                removeClass(from, modSideOpened);
+                addClass(from, modSideClosed);
+                removeClass(to, modSideClosed);
+                addClass(to, modSideOpened);
+            }
+
+            return this;
+        },
+
+        _getLangFromHash: function() {
+            var lang = document.location.hash.match(/(\w{2})/);
+            return lang ? lang[1] : '';
+        }
+    };
+
+    function addClass(elem, className) {
+        if (!hasClass(elem, className)) {
+            elem.className += ' ' + className;
+        }
+    }
+
+    function removeClass(elem, className) {
+        if (!hasClass(elem, className)) {
+            return;
+        }
+
+        var classes = elem.className.split(/\s/);
+        var idx = classes.indexOf(className);
+
+        classes.splice(idx, 1);
+
+        elem.className = classes.join(' ');
+    }
+
+    function hasClass(elem, className) {
+        return elem.className.split(/\s/).indexOf(className) !== -1;
+    }
+}());
+
+/* end: ../../libs/bem-vcard-enb/blocks/card/card.js */
+/* begin: ../../libs/bem-vcard-enb/blocks/metrika/metrika.js */
+var Metrika = {
+    init: function(id) {
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w['yaCounter' + id] = new Ya.Metrika({
+                        id: id,
+                        webvisor:true,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true,
+                        trackHash:true
+                    });
+                } catch(e) { }
+            });
+
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else { f(); }
+        })(document, window, "yandex_metrika_callbacks")
+    }
+};
+
+/* end: ../../libs/bem-vcard-enb/blocks/metrika/metrika.js */
