81f65b4f1300876913649377822ba2c37e9015db
dima117 <dima117a@gmail.com>
2016-10-18 21:14:22 +0300

    - запретил wildcards в роутинге;
    - теперь базовый контроллер возвращает данные для элементов навигации;
    - настройка applyTemplates для базового контроллера;

--- SiteEngine/Controllers/BaseController.cs
+++ SiteEngine/Controllers/BaseController.cs
@@ -1,24 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Bem;
 using System.Web.Mvc;
+using SiteEngine.Models;
 
 namespace SiteEngine.Controllers
 {
     public abstract class BaseController : Controller
     {
+        protected bool IsCurrentUrl(string url)
+        {
+            return Request.RawUrl.Trim('/').Equals(url.Trim('/'));
+        }
+
+        private object GetNavigation()
+        {
+            return Navigation.Items
+                .Select(item => new { item.content, item.url, selected = IsCurrentUrl(item.url) })
+                .ToArray();
+        }
+
         public ActionResult SitePage(object bemjson)
         {
             var obj = new
             {
                 block = "layout",
+                navigation = GetNavigation(),
                 content = bemjson
             };
 
-            return Json(obj, JsonRequestBehavior.AllowGet);
-            //return new BemhtmlResult(obj);
+            return AppSettings.ApplyTemplates
+                ? (ActionResult)new BemhtmlResult(obj)
+                : Json(obj, JsonRequestBehavior.AllowGet);
         }
     }
 }
